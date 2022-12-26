630aa854a2e3df2ff2800eada8253d67e642b62f
Igor Nikolaev <igor01131987@gmail.com>
2022-12-26 18:48:40 +0300

    Project setup

---
+++ HomeWork12/Sourses/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  HomeWork12
+//
+//  Created by Игорь Николаев on 26.12.2022.
+//
+
+import UIKit
+
+//MARK: - UI Elements
+
+//MARK: - LifeCycle
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+//MARK: - Setups
+
+    private func setupView() {
+
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+
+//MARK: - Actions
+
+}
+
